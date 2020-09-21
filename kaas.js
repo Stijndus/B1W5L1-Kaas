edf3fd44d549afba8a63120aedc42a65e01dcb0d
Stijn <99053054@mydavinci.nl>
2020-09-21 11:58:31 +0200

    added .toLowerCase after every prompt

--- kaas.js
+++ kaas.js
@@ -8,33 +8,33 @@ if(cheese === "ja"){
     if(cheese === "ja"){
         cheese = prompt("Is de kaas belachelijk duur?").toLowerCase()
         if(cheese === "ja"){
-            alert("Emmenthaler")
+            alert("Emmenthaler");
         } else if(cheese === "nee") {
-            alert("Leerdammer")
+            alert("Leerdammer");
         }
     } else if(cheese === "nee" ) {
         cheese = prompt("Is de kaas zo hard als een steen?").toLowerCase()
             if(cheese === "ja"){
-                alert("Parmigiano Reggiano")
+                alert("Parmigiano Reggiano");
             } else if (cheese === "nee"){
-                alert("Goudse kaas")
+                alert("Goudse kaas");
             }
     }
 }else if(cheese === "nee"){
-    cheese = prompt("Heeft de kaas blauwe schimmels?")
+    cheese = prompt("Heeft de kaas blauwe schimmels?").toLowerCase()
     if(cheese === "ja"){
-        cheese = prompt("Heeft de kaas een korst?")
+        cheese = prompt("Heeft de kaas een korst?").toLowerCase()
         if (cheese === "ja"){
-            alert("Bleu de Rochbaron")
+            alert("Bleu de Rochbaron");
         } else if(cheese === "nee"){
-            alert("Fourme d'Ambert")
+            alert("Fourme d'Ambert");
         }
     } else if(cheese === "nee"){
-        cheese = prompt("Heeft de kaas een korst?")
+        cheese = prompt("Heeft de kaas een korst?").toLowerCase()
         if(cheese === "ja"){
-            alert("Camembert")
+            alert("Camembert");
         } else if(cheese === "nee"){
-            alert("Mozzarella")
+            alert("Mozzarella");
         }
     }
 }
