8e2b8dd67b248b7a69d5a38826f22d084b805e5e
Stijn <99053054@mydavinci.nl>
2020-09-21 11:46:57 +0200

    Added Gaten path

--- kaas.js
+++ kaas.js
@@ -1,8 +1,23 @@
 //Stijn Dusseldorp
 //Software Developer
 
-if (prompt("Is de kaas geel?") == "Ja"){
-    prompt("Heeft de kaas gaten")
-}else{
-    prompt("Heeft de kaas blauwe schimmels?")
+var cheese = prompt("Is de kaas geel?").toLowerCase();
+
+if(cheese === "ja"){
+    cheese = prompt("Zitten er gaten in?").toLowerCase()
+    if(cheese === "ja"){
+        cheese = prompt("Is de kaas belachelijk duur?").toLowerCase()
+        if(cheese === "ja"){
+            alert("Emmenthaler")
+        } else if(cheese === "nee") {
+            alert("Leerdammer")
+        }
+    } else if(cheese === "nee" ) {
+        cheese = prompt("Is de kaas zo hard als een steen?").toLowerCase()
+            if(cheese === "ja"){
+                alert("Parmigiano Reggiano")
+            } else if (cheese === "nee"){
+                alert("Goudse kaas")
+            }
+    }
 }
